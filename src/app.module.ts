eb97a67e66b6e81eeb5307f66f6ad8bc9c228581
Pedro Pimienta M. <pedro.pimienta.morales@gmail.com>
2025-04-05 10:20:16 -0500

    refactor: update Prettier configuration and enhance Docker Compose setup; add Mongoose integration and user schema

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,31 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { UserSchema, UserSchemaFactory } from './domain/entitties/user.schema';
+
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 @Module({
-  imports: [],
-  controllers: [],
-  providers: [],
+    imports: [
+        ConfigModule.forRoot({
+            isGlobal: true,
+            envFilePath: '.env',
+        }),
+
+        MongooseModule.forRootAsync({
+            imports: [ConfigModule],
+            useFactory: (configService: ConfigService) => ({
+                uri: `mongodb://${configService.get<string>('DB_USER')}:${configService.get<string>('DB_PASSWORD')}@${configService.get<string>('DB_HOST')}:${configService.get<number>('DB_PORT')}/${configService.get<string>('DB_NAME')}?authSource=admin`,
+            }),
+            inject: [ConfigService],
+        }),
+        MongooseModule.forFeature([
+            { name: UserSchema.name, schema: UserSchemaFactory },
+        ]),
+    ],
+    controllers: [],
+    providers: [],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+    console.log(
+        `mongodb://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}?authSource=admin`,
+    );
+
+    const app = await NestFactory.create(AppModule);
+    await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
